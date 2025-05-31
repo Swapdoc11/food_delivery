783666fcd505ea8684560e3767b978d6974c480f
Swapdoc11 <swapniljadhav.maratha@gmail.com>
2025-05-31 11:03:21 +0530

    fix: update API base URL and adjust ESLint rules for react-hooks

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = `https://food-delivery-lilac-eight.vercel.app/api`
+export const API_BASE_URL = `https://food-delivery-khaki-eta.vercel.app/api`
 // export const API_BASE_URL = `http://localhost:3000/api`
